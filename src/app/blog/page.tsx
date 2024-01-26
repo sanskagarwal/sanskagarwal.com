faf129ffbc9775e0303efc0c8b4863d8f5194f5b
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2024-01-26 16:11:29 +0530

    add tag sorting system

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,24 +1,54 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import useSWR from "swr";
 import { BulletList } from "react-content-loader";
 import Link from "next/link";
 
 import { fetcher } from "@/app/_dataprovider/ClientDataProvider";
 import { Blog } from "../_models/Blog";
-import { Card, CardContent } from "semantic-ui-react";
+import { Card, CardContent, Label } from "semantic-ui-react";
 
 const BlogList: React.FC = () => {
+    const [activeLabel, setActiveLabel] = useState("all");
+
     const {
         data: blogList,
         isLoading,
         error,
     } = useSWR<Blog[]>("/api/blogs", fetcher);
 
+    const changeActiveLabel = (e: any) => {
+        setActiveLabel(e.target.innerText);
+    };
+
+    const createLabels = (blogList: Blog[]) => {
+        const labels: string[] = [];
+        blogList.map((blog: Blog) => {
+            if (!labels.includes(blog.category)) {
+                labels.push(blog.category);
+            }
+        });
+
+        labels.push("all");
+        labels.sort();
+
+        return (
+            <div>
+                {labels.map((label: string) => {
+                    return (
+                        <Label key={label} as="a" active={label === activeLabel} onClick={changeActiveLabel}>
+                            {label}
+                        </Label>
+                    );
+                })}
+            </div>
+        );
+    };
+
     return (
         <>
-            <p>List of Blogs</p>
+            <p>Blogs</p>
             {error && <div>Failed to load blogs</div>}
             {isLoading && (
                 <Card>
@@ -27,8 +57,13 @@ const BlogList: React.FC = () => {
                     </CardContent>
                 </Card>
             )}
+            {blogList && createLabels(blogList)}
             {blogList &&
                 blogList.map((blog: Blog) => {
+                    if (activeLabel !== "all" && blog.category !== activeLabel) {
+                        return;
+                    }
+
                     return (
                         <div key={blog.id.toString()} className="ui card">
                             <div className="content">
