3c720dc63fce85dd488a0a5e481b776554f2c627
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2024-12-09 15:59:13 +0530

    create new strapi admin

--- content/config/plugins.ts
+++ content/config/plugins.ts
@@ -1,16 +1,1 @@
-export default ({ env }) => {
-    return {
-        upload: {
-            config: {
-                provider: "strapi-provider-upload-azure-storage",
-                providerOptions: {
-                    account: env("STORAGE_ACCOUNT"),
-                    accountKey: env("STORAGE_ACCOUNT_KEY"),
-                    containerName: env("STORAGE_CONTAINER_NAME"),
-                    defaultPath: "assets",
-                    cdnBaseURL: env("STORAGE_CDN_URL"),
-                },
-            },
-        },
-    };
-};
+export default () => ({});
