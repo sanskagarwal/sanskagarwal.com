8865462c214107fc079c12b768f4d624d647e53d
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2024-12-09 16:21:55 +0530

    add storage

--- content/config/plugins.ts
+++ content/config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-azure-storage",
+      providerOptions: {
+        authType: env("STORAGE_AUTH_TYPE", "default"),
+        account: env("STORAGE_ACCOUNT"),
+        accountKey: env("STORAGE_ACCOUNT_KEY"), //either account key or sas token is enough to make authentication
+        containerName: env("STORAGE_CONTAINER_NAME"),
+        defaultPath: "assets",
+        cdnBaseURL: env("STORAGE_CDN_URL"), // optional
+      },
+    },
+  },
+});
