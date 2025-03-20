c4fb6bd5bcac5f25b5a71e4d687a85df4e280c83
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2025-03-21 00:10:07 +0530

    add share blog utility

--- src/app/blog/[url]/page.tsx
+++ src/app/blog/[url]/page.tsx
@@ -3,6 +3,17 @@
 import React, { useEffect } from "react";
 import useSWR from "swr";
 import { List } from "react-content-loader";
+import {
+    EmailIcon,
+    EmailShareButton,
+    LinkedinIcon,
+    LinkedinShareButton,
+    RedditIcon,
+    RedditShareButton,
+    TwitterShareButton,
+    XIcon,
+} from "react-share";
+import readingTime from "reading-time";
 
 import { fetcher } from "@/app/_dataprovider/ClientDataProvider";
 import { Blog } from "@/app/_models/Blog";
@@ -102,18 +113,54 @@ const BlogPage: React.FC<{ params: Params }> = ({ params }) => {
                     <div>
                         <div className="ui text">
                             <h1 className="ui header">{blog.title}</h1>
-                            <h6 className="ui header">
-                                Publish Date:{" "}
-                                {new Date(blog.published_at).toLocaleDateString(
-                                    undefined,
-                                    {
-                                        year: "numeric",
-                                        month: "short",
-                                        day: "numeric",
-                                    }
-                                )}
-                            </h6>
+                            <p className="ui text-gray-600">
+                                {`${new Date(
+                                    blog.published_at
+                                ).toLocaleDateString(undefined, {
+                                    weekday: "long",
+                                    year: "numeric",
+                                    month: "long",
+                                    day: "numeric",
+                                })} | ${readingTime(blog.content).text}`}
+                            </p>
                             {getHTML(blog.content)}
+                        </div>
+                        <div className="ui center aligned segment">
+                            <div className="inline">
+                                <h4 className="ui header">Share this blog</h4>
+                                <EmailShareButton
+                                    url={window.location.href}
+                                    subject={`Check out this blog: ${blog.title}`}
+                                    body={`I found this blog interesting and thought you might like it too! Link to the blog: ${window.location.href}`}
+                                    className="ui button"
+                                >
+                                    <EmailIcon size={32} round />
+                                </EmailShareButton>
+                                <TwitterShareButton
+                                    url={window.location.href}
+                                    title={blog.title}
+                                    hashtags={[blog.category]}
+                                    className="ui button"
+                                >
+                                    <XIcon size={32} round />
+                                </TwitterShareButton>
+                                <LinkedinShareButton
+                                    url={window.location.href}
+                                    title={blog.title}
+                                    summary={blog.summary}
+                                    source="Sanskar Agarwal's Blog"
+                                    className="ui button"
+                                >
+                                    <LinkedinIcon size={32} round />
+                                </LinkedinShareButton>
+                                <RedditShareButton
+                                    url={window.location.href}
+                                    title={blog.title}
+                                    className="ui button"
+                                >
+                                    <RedditIcon size={32} round />
+                                </RedditShareButton>
+                            </div>
                         </div>
                         <br />
                     </div>
