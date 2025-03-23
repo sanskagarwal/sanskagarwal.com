ea62bbfe9aad2c8e3b0f503d97ee8f2c8866f37c
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2025-03-23 15:01:55 +0530

    add batching logic

--- notes-llm/modules/openai.py
+++ notes-llm/modules/openai.py
@@ -10,6 +10,7 @@ load_dotenv()
 
 # Initialize the Azure OpenAI client
 deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+model_encoding = tiktoken.encoding_for_model(deployment)
 client = AzureOpenAI(
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
@@ -18,7 +19,7 @@ client = AzureOpenAI(
 
 # Define limits
 total_token_limit = 4096
-input_token_limit = 1500
+input_token_limit = 1600
 
 # Read prompt from file
 with open("prompt.txt", "r") as file:
@@ -46,24 +47,39 @@ def get_encoded_image(file_prefix, images_path):
     return encoded_images
 
 
-def add_image_to_prompt(encoded_images):
+def add_image_to_prompt(encoded_images, image_index):
     """
     Adds an image to the prompt text.
     """
 
     chat_prompt = [
-        {"role": "system", "content": [{"type": "text", "text": prompt_text}]}
+        {"role": "system", "content": [{"type": "text", "text": prompt_text}]},
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": f"We have converted {image_index} pages so far.",
+                }
+            ],
+        },
     ]
 
-    model_encoding = tiktoken.encoding_for_model(deployment)
-    input_token_count = (
-        4 + 2
-    )  # Assuming 4 tokens for the <imsystem> message and 2 tokens for the <imassistant> message
-    input_token_count += len(model_encoding.encode(prompt_text))
+    input_token_count = 2  # Assuming 2 tokens for the <imassistant> message
+    for message in chat_prompt:
+        if "content" in message:
+            for content_item in message["content"]:
+                if content_item["type"] == "text":
+                    input_token_count += len(
+                        model_encoding.encode(content_item["text"])
+                    )
+
+        input_token_count += 4  # Assuming 4 tokens for the <imuser> message
 
     # Add the encoded images to the prompt based on the token limit
     user_content = []
-    for i, base64_image in enumerate(encoded_images):
+    for i in range(image_index, len(encoded_images)):
+        base64_image = encoded_images[i]
         image_message = {
             "type": "image_url",
             "image_url": {
@@ -74,13 +90,16 @@ def add_image_to_prompt(encoded_images):
 
         input_token_count += 4  # Assuming 4 tokens for the <imuser> message
         input_token_count += 170  # Assuming 170 tokens for each high-detailed image URL
-        if input_token_count > total_token_limit - input_token_limit:
+        if input_token_count > input_token_limit:
+            # Remove the last image
+            i -= 1
+            input_token_count -= 4
+            input_token_count -= 170
             break
 
         user_content.append(image_message)
 
     chat_prompt.append({"role": "user", "content": user_content})
-
     print(f"Total tokens in prompt: {input_token_count}")
     return i, chat_prompt
 
@@ -90,34 +109,45 @@ def send_images_to_openai(file_prefix, images_path):
     Sends a batch of images to Azure OpenAI with a prompt and retrieves the response.
     """
 
-    # Get image path
+    batch_num = 1
+    image_index = 0
     encoded_images = get_encoded_image(file_prefix, images_path)
     if not encoded_images:
         return None
 
-    # Create the message with the encoded images ensuring token limit
-    i, messages = add_image_to_prompt(encoded_images)
-    if i == 0:
-        print("No images to send.")
-        return None
+    while image_index < len(images_path):
+        print(
+            f"-----------------------------------\nProcessing batch {batch_num}..."
+        )
 
-    print(f"Sending {i} images to OpenAI API...")
-    completion = client.chat.completions.create(
-        model=deployment,
-        messages=messages,
-        max_tokens=total_token_limit,
-        temperature=0.7,
-        top_p=0.95,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=None,
-        stream=False,
-    )
+        # Create the message with the encoded images ensuring token limit
+        new_image_index, messages = add_image_to_prompt(encoded_images, image_index)
+        images_to_send = new_image_index - image_index + 1
+        if images_to_send == 0:
+            print("No images to send.")
+            return None
 
-    response = completion.choices[0].message.content
-    save_response_as_md(response, file_prefix, 0)
+        print(f"Sending {images_to_send} images to OpenAI API...")
+        # completion = client.chat.completions.create(
+        #     model=deployment,
+        #     messages=messages,
+        #     max_tokens=total_token_limit,
+        #     temperature=0.7,
+        #     top_p=0.95,
+        #     frequency_penalty=0,
+        #     presence_penalty=0,
+        #     stop=None,
+        #     stream=False,
+        # )
 
-    # TODO Logic to wait some time before sending the next batch
+        # response = completion.choices[0].message.content
+        # print(f"Response tokens: {len(model_encoding.encode(response))}")
+        # save_response_as_md(response, file_prefix, batch_num)
+
+        # Wait for the next batch
+        print("Waiting for the next batch...")
+        batch_num += 1
+        image_index = new_image_index + 1
 
 
 def save_response_as_md(response, filename, batch_num, output_folder="output"):
@@ -129,7 +159,7 @@ def save_response_as_md(response, filename, batch_num, output_folder="output"):
         f"Saving LLM response\n-----------------------------------\n{response}\n-----------------------------------"
     )
     os.makedirs(output_folder, exist_ok=True)
-    md_file_path = os.path.join(output_folder, f"{filename}_{batch_num + 1}.md")
+    md_file_path = os.path.join(output_folder, f"{filename}_{batch_num}.md")
     with open(md_file_path, "w") as md_file:
         md_file.write(response)
 
