18a757eeeeee66a8e9730731fd417c8b482c446c
Sanskar Agarwal <sanskaragarwal.500@gmail.com>
2025-10-23 00:43:18 +0530

    remove azure storage plugin

--- content/config/plugins.ts
+++ content/config/plugins.ts
@@ -1,15 +1,2 @@
 export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: "strapi-provider-upload-azure-storage",
-      providerOptions: {
-        authType: env("STORAGE_AUTH_TYPE", "default"),
-        account: env("STORAGE_ACCOUNT"),
-        accountKey: env("STORAGE_ACCOUNT_KEY"), //either account key or sas token is enough to make authentication
-        containerName: env("STORAGE_CONTAINER_NAME"),
-        defaultPath: "assets",
-        cdnBaseURL: env("STORAGE_CDN_URL"), // optional
-      },
-    },
-  },
 });
